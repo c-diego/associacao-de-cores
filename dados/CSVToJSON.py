9f3daf943aab8ca2da682461d18d60a41f790ec4
c-diego <diegocorte02@hotmail.com>
2023-12-04 23:21:53 -0300

    Deletando arquivos JSON. Converter salva um JSON sumarizado

--- dados/CSVToJSON.py
+++ dados/CSVToJSON.py
@@ -1,55 +1,34 @@
-from collections import defaultdict
-import csv, json
+import json, csv
 
 def read_csv(path):
-  data_list = []
+  questionaries = []
 
   with open(path, newline='\n') as file:
     reader = csv.DictReader(file, delimiter=',', quotechar='"')
-    data_list = list(reader)
+    questionaries = list(reader)
   
-  return data_list
+  return questionaries
 
-def group_age_2003(age):
-  if age <= 18:
-    age_group = 'Até 18'
-  elif age <= 24:
-    age_group = '19 a 24'
-  elif age <= 35:
-    age_group = '25 a 35'
-  elif age <= 50:
-    age_group = '36 a 50'
-  elif age <= 61:
-    age_group = '51 a 69'
-  else:
-    age_group = 'mais de 70'
-
-  return age_group
-
-
-def create_dict(data_list):
-  data_dict = defaultdict(list)
-
-  for participant_answer in data_list:
-    for question, answer in participant_answer.items():
-        
-        if (question == 'What is your age?'):
-          answer = group_age_2003(int(answer))
-
-        if (answer.isdigit()):
-          answer = int(answer)
-        
-        data_dict[question].append(answer)
-
-  return data_dict
-
-def save_dict_to_json(data_dict, path):
+def save_to_json(data_dict, path):
   with open(path, 'w', encoding='UTF-8') as file:
     json.dump(data_dict, file, ensure_ascii=False, indent=2)
 
-csv_file = '2003.csv'
-json_file = '2003-2.json'
+questionaries = read_csv('./dados/2023.csv')
+total = {}
 
-data_list = read_csv(csv_file)
-data_dict = create_dict(data_list)
-save_dict_to_json(data_dict, json_file)
+for questions in questionaries[0].keys():
+    total[questions] = []
+
+for questionary in questionaries:
+  for question, answer in questionary.items():
+    total[question].append(answer)
+
+for question, answers in total.items():
+  count = {}
+
+  for answer in answers:
+    count[answer] = total[question].count(answer)
+
+  total[question] = count
+
+save_to_json(total, 'total2023.json')
