d545679649caf7fba4bee930faaa44a99d2f6c42
c-diego <diegocorte02@hotmail.com>
2024-02-12 00:41:15 -0300

    chore: separando dados para

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from '/vite.svg';
-import '@/App.css';
+import dados from '../dados.json';
 
 export default function App(): JSX.Element {
-  const [count, setCount] = useState(0);
+  const participantes = filtraParticipantes();
 
+  return <Participante participantes={participantes} />;
+}
+
+function Participante({
+  participantes
+}: {
+  participantes: React.ReactNode[][];
+}): JSX.Element {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
+      {participantes.map((participante, index) => (
+        <div
+          key={index}
+          style={{
+            border: '1px solid black',
+            padding: '10px'
           }}
         >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <h2>PARTICIPANTE {index + 1}</h2>
+          {participante.map((item, innerIndex) => (
+            <span key={innerIndex}>
+              {(innerIndex + 1) % 2 !== 0 ? (
+                dados['2023'].perguntas[innerIndex / 2]
+              ) : (
+                <span style={{ color: 'blue' }}> {item}</span>
+              )}
+              {(innerIndex + 1) % 2 === 0 && <br />}
+            </span>
+          ))}
+        </div>
+      ))}
     </>
   );
+}
+
+function filtraParticipantes(): any[] {
+  const participantes = [];
+
+  for (const participante of dados['2023'].dados) {
+    const perguntasRespostas = [];
+    for (const [pergunta, resposta] of Object.entries(participante)) {
+      perguntasRespostas.push(pergunta, resposta);
+    }
+    participantes.push(perguntasRespostas);
+  }
+
+  return participantes;
 }
