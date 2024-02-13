81fa6c38af725da3053a386713803a4357cd033e
c-diego <diegocorte02@hotmail.com>
2024-02-12 23:39:53 -0300

    chore: adicionando chartjs e o wrapper para o react

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,49 @@
-import dados from '../dados.json';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { dados2023 } from './dados/dados';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: [
+    'Orange',
+    'Yellow',
+    'Green',
+    'Black',
+    'Red',
+    'Blue',
+    'White',
+    'Purple',
+    'Brown',
+    'Grey'
+  ],
+  datasets: [
+    {
+      label: 'Número de pessoas que escolheram essa cor',
+      data: Object.values(dados2023[5]),
+      backgroundColor: Object.keys(dados2023[5]),
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 0.4
+    }
+  ]
+};
 
 export default function App(): JSX.Element {
-  const participantes = filtraParticipantes();
-
-  return <Participante participantes={participantes} />;
-}
-
-function Participante({
-  participantes
-}: {
-  participantes: React.ReactNode[][];
-}): JSX.Element {
   return (
-    <>
-      {participantes.map((participante, index) => (
-        <div
-          key={index}
-          style={{
-            border: '1px solid black',
-            padding: '10px'
-          }}
-        >
-          <h2>PARTICIPANTE {index + 1}</h2>
-          {participante.map((item, innerIndex) => (
-            <span key={innerIndex}>
-              {(innerIndex + 1) % 2 !== 0 ? (
-                dados['2023'].perguntas[innerIndex / 2]
-              ) : (
-                <span style={{ color: 'blue' }}> {item}</span>
-              )}
-              {(innerIndex + 1) % 2 === 0 && <br />}
-            </span>
-          ))}
-        </div>
-      ))}
-    </>
+    <div
+      style={{
+        width: '600px'
+      }}
+    >
+      <Pie data={data} />
+      <Pie data={data} />
+    </div>
   );
-}
-
-function filtraParticipantes(): any[] {
-  const participantes = [];
-
-  for (const participante of dados['2023'].dados) {
-    const perguntasRespostas = [];
-    for (const [pergunta, resposta] of Object.entries(participante)) {
-      perguntasRespostas.push(pergunta, resposta);
-    }
-    participantes.push(perguntasRespostas);
-  }
-
-  return participantes;
 }
